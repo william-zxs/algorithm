567da730b1e5402f6608b56dd9c3ca684c235d6a
william <william_zxs@163.com>
2021-10-25 22:37:33 +0800

    add 堆排序

--- sort.go
+++ sort.go
@@ -76,10 +76,62 @@ func merge(left, right []int) (result []int) {
 	return result
 }
 
-func main() {
-	data := []int{2, 1, 3, 6, 4, 10, 7}
+/*
 
-	res := QuickSort(data)
+一般都用数组来表示堆，i结点的父结点下标就为(i-1)/2。
+它的左右子结点下标分别为2 * i + 1和2 * i + 2。如第0个结点左右子结点下标分别为1和2。
+
+对于叶子节点，不用调整次序，根据满二叉树的性质，叶子节点比内部节点的个数多1.所以i=n/2 -1 ，不用从n开始。
+*/
+//堆排序
+func HeapSort(nums []int) []int {
+	//思路 先构建一个大根堆，交换根节点和最后一个节点，继续恢复大根堆
+
+	//构建大根堆
+	for i := len(nums)/2 - 1; i >= 0; i-- {
+		sink(nums, i, len(nums))
+	}
+
+	//排序
+	// 最后一个和根节点置换
+	for i := len(nums) - 1; i >= 1; i-- {
+		//置换i 和 0
+		swap(nums, 0, i)
+
+		//恢复大根堆
+		sink(nums, 0, i)
+	}
+	return nums
+}
+
+func sink(nums []int, i, len int) {
+	for {
+		l := 2*i + 1
+		r := 2*i + 2
+		idx := i
+
+		if l < len && nums[l] > nums[i] {
+			idx = l
+		}
+
+		if r < len && nums[r] > nums[idx] {
+			idx = r
+		}
+		if idx == i {
+			break
+		}
+
+		swap(nums, i, idx)
+
+		i = idx
+	}
+
+}
+
+func main() {
+	data := []int{5, 1, 3, 2, 8, 3, 5, 6, 1, 3, 4, 5, 7, 7, 10, 3123, 4, 3, 535, 542, 12, 1, 293}
+
+	res := HeapSort(data)
 	fmt.Println("==res==:", res)
 
 }
