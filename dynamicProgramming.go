c69dacddb62551c3eb05f014acaed91df10ef0e4
william <william_zxs@163.com>
2021-10-28 11:39:28 +0800

    63. 不同路径 II //70. 爬楼梯

--- dynamicProgramming.go
+++ dynamicProgramming.go
@@ -71,6 +71,7 @@ func minPathSum(grid [][]int) int {
 }
 
 // 62. 不同路径
+// 也可以用数学的方式，直接计算排列组合
 func uniquePaths(m int, n int) int {
 	// [[1,2],[1,2],[1,2]]  m =3 n =2
 	// 重点是找到状态转移方程
@@ -97,9 +98,59 @@ func uniquePaths(m int, n int) int {
 	return path[m-1][n-1]
 }
 
+// 63. 不同路径 II
+func uniquePathsWithObstacles(obstacleGrid [][]int) int {
+	m := len(obstacleGrid)
+	n := len(obstacleGrid[0])
+	path := make([][]int, m)
+	for i := range path {
+		path[i] = make([]int, n)
+	}
+
+	for x := 0; x < m; x++ {
+		for y := 0; y < n; y++ {
+			if obstacleGrid[x][y] == 1 {
+				path[x][y] = 0
+				continue
+			}
+			if x == 0 && y == 0 {
+				path[0][0] = 1
+			} else if x == 0 {
+				path[0][y] = path[0][y-1]
+			} else if y == 0 {
+				path[x][0] = path[x-1][0]
+			} else {
+				path[x][y] = path[x][y-1] + path[x-1][y]
+			}
+		}
+	}
+	return path[m-1][n-1]
+}
+
+//70. 爬楼梯
+func climbStairs(n int) int {
+	/*
+			假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+
+		每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+
+		注意：给定 n 是一个正整数。
+	*/
+
+	//动态规划，要把大问题转化成小问题，先找到转移方程
+	dp := make([]int, n+1)
+	dp[0] = 1
+	dp[1] = 1
+	for i := 2; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+
+	return dp[n]
+}
 func main() {
 	// triangle := [][]int{{1, 3, 1}, {1, 5, 1}, {4, 2, 1}}
 	// res := minPathSum(triangle)
-	res := uniquePaths(3, 7)
+	// res := uniquePaths(3, 7)
+	res := climbStairs(10)
 	fmt.Println("==res==:", res)
 }
