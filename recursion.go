126bece36c623c207fab45cd842f72a76c8e931c
william <william_zxs@163.com>
2021-11-02 22:41:46 +0800

    add 95. 不同的二叉搜索树 II 509. 斐波那契

--- recursion.go
+++ recursion.go
@@ -55,18 +55,83 @@ func swapPairs2(head *ListNode) *ListNode {
 	return next
 }
 
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// 95. 不同的二叉搜索树 II
+func generateTrees(n int) []*TreeNode {
+	//二叉搜索树的特点是  左节点值小于根节点，右节点值大于根节点,左子树和右子树同样也是二叉搜索树
+
+	if n == 0 {
+		return nil
+	}
+	return generate(1, n)
+}
+func generate(start, end int) []*TreeNode {
+	if start > end {
+		return []*TreeNode{nil}
+	}
+	treeNodes := make([]*TreeNode, 0)
+	for i := start; i <= end; i++ {
+
+		leftNodes := generate(start, i-1)
+		rightNodes := generate(i+1, end)
+		for j := 0; j < len(leftNodes); j++ {
+			for k := 0; k < len(rightNodes); k++ {
+
+				root := &TreeNode{Val: i}
+				root.Left = leftNodes[j]
+				root.Right = rightNodes[k]
+				treeNodes = append(treeNodes, root)
+				fmt.Println("==treeNodes==", treeNodes)
+			}
+		}
+	}
+	return treeNodes
+}
+
+// 509. 斐波那契数
+
+func fib(n int) int {
+
+	var help func(n int) int
+	cache := make(map[int]int)
+
+	help = func(n int) int {
+		if num, ok := cache[n]; ok {
+			return num
+		}
+		if n == 0 {
+			return 0
+		}
+		if n == 1 {
+			return 1
+		}
+		res := help(n-1) + help(n-2)
+		cache[n] = res
+		return res
+	}
+	return help(n)
+}
+
 func main() {
 
-	node2 := &ListNode{
-		Val:  2,
-		Next: nil,
-	}
-	node1 := &ListNode{
-		Val:  1,
-		Next: node2,
-	}
-	head := node1
+	// node2 := &ListNode{
+	// 	Val:  2,
+	// 	Next: nil,
+	// }
+	// node1 := &ListNode{
+	// 	Val:  1,
+	// 	Next: node2,
+	// }
+	// head := node1
 
-	res := swapPairs2(head)
+	// res := swapPairs2(head)
+
+	// res := generateTrees(3)
+	res := fib(3)
 	fmt.Println("==res==:", res)
 }
