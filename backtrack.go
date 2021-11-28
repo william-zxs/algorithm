765e54755e0ac561d5a4c9140eeb8924061fa850
william <william_zxs@163.com>
2021-11-28 11:22:10 +0800

    add 90. 子集 II 46. 全排列

--- backtrack.go
+++ backtrack.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 /*
 回溯算法
@@ -27,9 +30,65 @@ func backtrack(nums []int, pos int, list []int, result *[][]int) {
 	}
 }
 
+// 90. 子集 II
+func subsetsWithDup(nums []int) [][]int {
+	result := make([][]int, 0)
+	list := make([]int, 0)
+	sort.Ints(nums)
+	backtrackWithDup(nums, 0, list, &result)
+	return result
+}
+
+func backtrackWithDup(nums []int, pos int, list []int, result *[][]int) {
+	//保存路径
+	data := make([]int, len(list))
+	copy(data, list)
+	*result = append(*result, data)
+	for i := pos; i < len(nums); i++ {
+		//剪枝
+		if i != pos && nums[i] == nums[i-1] {
+			continue
+		}
+		//做选择
+		list = append(list, nums[i])
+		//递归
+		backtrackWithDup(nums, i+1, list, result)
+		//撤销选择
+		list = list[:len(list)-1]
+	}
+}
+
+// 46. 全排列
+func permute(nums []int) [][]int {
+	result := make([][]int, 0)
+	list := make([]int, 0)
+	backtrackPermute(nums, list, &result)
+	return result
+}
+
+func backtrackPermute(nums []int, list []int, result *[][]int) {
+	if len(list) == len(nums) {
+		data := make([]int, len(list))
+		copy(data, list)
+		*result = append(*result, data)
+		return
+	}
+out:
+	for i := 0; i < len(nums); i++ {
+		for y := 0; y < len(list); y++ {
+			if nums[i] == list[y] {
+				continue out
+			}
+		}
+		list = append(list, nums[i])
+		backtrackPermute(nums, list, result)
+		list = list[:len(list)-1]
+	}
+}
+
 func main() {
 
 	data := []int{1, 2, 3}
-	res := subsets(data)
+	res := permute(data)
 	fmt.Println("==res==", res)
 }
