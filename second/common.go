6586171263086fbdbf0f8ef6f29d23d6857cd2c5
william <william_zxs@163.com>
2021-12-10 15:56:13 +0800

    复习

---
+++ second/common.go
@@ -0,0 +1,20 @@
+package main
+
+// 28. 实现 strStr()
+func strStr(haystack string, needle string) int {
+	if len(needle) == 0 {
+		return 0
+	}
+
+	for i := 0; i < len(haystack)-len(needle)+1; i++ {
+		for j := 0; j < len(needle); j++ {
+			if haystack[i+j] != needle[j] {
+				break
+			}
+			if j == len(needle)-1 {
+				return i
+			}
+		}
+	}
+	return -1
+}
