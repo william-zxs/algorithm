f0b52dee7f87377774c4757335aee1b6e801ecea
william <william_zxs@163.com>
2022-01-18 16:29:28 +0800

    add 155. 最小栈

--- second/common.go
+++ second/common.go
@@ -756,3 +756,49 @@ func copyRandomList(head *Node) *Node {
 	}
 	return copyHead
 }
+
+/*
+栈和队列
+栈的特点是后入先出
+栈常用于DFS
+队列常用于BFS
+*/
+
+// 155. 最小栈
+type MinStack struct {
+	stack    []int
+	minStack []int
+}
+
+func Constructor() MinStack {
+
+	return MinStack{stack: make([]int, 0), minStack: make([]int, 0)}
+}
+
+func (this *MinStack) Push(val int) {
+	this.stack = append(this.stack, val)
+	if len(this.minStack) == 0 {
+		this.minStack = append(this.minStack, val)
+	} else {
+
+		if val < this.minStack[len(this.minStack)-1] {
+			this.minStack = append(this.minStack, val)
+		} else {
+			this.minStack = append(this.minStack, this.minStack[len(this.minStack)-1])
+		}
+	}
+
+}
+
+func (this *MinStack) Pop() {
+	this.stack = this.stack[:len(this.stack)-1]
+	this.minStack = this.minStack[:len(this.minStack)-1]
+}
+
+func (this *MinStack) Top() int {
+	return this.stack[len(this.stack)-1]
+}
+
+func (this *MinStack) GetMin() int {
+	return this.minStack[len(this.minStack)-1]
+}
