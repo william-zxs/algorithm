e20eb1cb1ceefb650dd9a0f992cb2c3241d160b2
william <william_zxs@163.com>
2022-02-15 22:48:06 +0800

    update

--- second/common.go
+++ second/common.go
@@ -27,6 +27,7 @@ import (
 338. 比特位计数
 190. 颠倒二进制位
 35. 搜索插入位置
+153. 寻找旋转排序数组中的最小值  边界值和条件需要注意
 */
 
 type TreeNode struct {
@@ -1253,4 +1254,59 @@ func firstBadVersion(n int) int {
 	}
 
 	return res
+}
+
+// 153. 寻找旋转排序数组中的最小值
+func findMin(nums []int) int {
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	l, r := 0, len(nums)-1
+	for l < r {
+		mid := l + (r-l)/2
+		if nums[mid] < nums[r] {
+			r = mid
+		} else {
+			l = mid + 1
+		}
+	}
+
+	return nums[r]
+}
+func findMin2(nums []int) int {
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	l, r := 0, len(nums)-1
+	for l+1 < r {
+		mid := l + (r-l)/2
+		if nums[l] < nums[r] {
+			return nums[l]
+		} else if nums[mid] > nums[l] {
+			l = mid + 1
+		} else if nums[mid] < nums[r] {
+			r = mid
+		}
+	}
+	if num[l] < nums[r] {
+		return num[l]
+	} else {
+		return num[r]
+	}
+}
+
+// 154. 寻找旋转排序数组中的最小值 II
+func findMinII(nums []int) int {
+	low, high := 0, len(nums)-1
+	for low < high {
+		pivot := low + (high-low)/2
+		if nums[pivot] < nums[high] {
+			high = pivot
+		} else if nums[pivot] > nums[high] {
+			low = pivot + 1
+		} else {
+			high--
+		}
+	}
+	return nums[low]
 }
