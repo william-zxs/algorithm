160b0b9b3561ef5127e7a3c7ac73fee5b0983d3c
william <william_zxs@163.com>
2022-04-16 15:07:03 +0800

    add sort quicksort mergesort

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	data := []int{5, 1, 2, 4, 8, 3, 6, 7}
-	algorithm.QuickSort(data)
+	//algorithm.QuickSort(data)
+	data = algorithm.MergeSort(data)
 	fmt.Println(data)
 }
--- sort.go
+++ sort.go
@@ -1,23 +1,19 @@
 package algorithm
 
 //快排
-func QuickSort(nums []int) []int {
-	//快排的思路是选取最后一个值为基准值r，遍历list和基准值比较，
-	//一个记录最左边比基准值大的一个 j，当前遍历i比基准值要小就交换swap(j,i),
-	//同时 j++,直到遍历完成，将基准值r和j互换，按照索引j的分成两份，递归
+//时间复杂度最好nlogn, 最差n2
+func QuickSort(nums []int) {
 	quickSort(nums, 0, len(nums)-1)
-	return nums
 }
-
 func quickSort(nums []int, start, end int) {
 	if start >= end {
 		return
 	}
 
-	b := nums[end]
+	endValue := nums[end]
 	j := start
 	for i := start; i < end; i++ {
-		if nums[i] < b {
+		if nums[i] < endValue {
 			swap(nums, j, i)
 			j++
 		}
@@ -26,6 +22,39 @@ func quickSort(nums []int, start, end int) {
 	quickSort(nums, start, j-1)
 	quickSort(nums, j+1, end)
 }
+
 func swap(nums []int, i, j int) {
 	nums[i], nums[j] = nums[j], nums[i]
+}
+
+//归并排序
+//时间复杂度是nlogn
+//如何用公式计算时间复杂度
+func MergeSort(nums []int) []int {
+	//不断二分，最后比较两值，合并成一个有序数组，返回，然后再合并left和right的返回
+
+	if len(nums) <= 1 {
+		return nums
+	}
+	m := len(nums) / 2
+	l := MergeSort(nums[:m])
+	r := MergeSort(nums[m:])
+	return merge(l, r)
+}
+
+func merge(left, right []int) []int {
+	res := []int{}
+	l, r := 0, 0
+	for l < len(left) && r < len(right) {
+		if left[l] < right[r] {
+			res = append(res, left[l])
+			l++
+		} else {
+			res = append(res, right[r])
+			r++
+		}
+	}
+	res = append(res, left[l:]...)
+	res = append(res, right[r:]...)
+	return res
 }
