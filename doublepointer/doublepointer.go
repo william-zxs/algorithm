69a71513cb8921d13eb337b85605837390a0cfe4
william <william_zxs@163.com>
2022-08-03 22:18:58 +0800

    add doublepointer

---
+++ doublepointer/doublepointer.go
@@ -0,0 +1,20 @@
+package doublepointer
+
+import (
+	"github.com/William-ZXS/algorithm/util"
+	"math"
+)
+
+//双指针
+//当两个指针同速同方向移动，也就是滑动窗口
+func MaxSum(cycle []int, N int) int {
+	sum := math.MinInt
+	for i, j := 0, N-1; j < len(cycle)*2-1; i, j = i+1, j+1 {
+		v := 0
+		for m := i; m <= j; m++ {
+			v += cycle[m%len(cycle)]
+		}
+		sum = util.Max(sum, v)
+	}
+	return sum
+}
---
+++ util/util.go
@@ -0,0 +1,8 @@
+package util
+
+func Max(m, n int) int {
+	if m > n {
+		return m
+	}
+	return n
+}
