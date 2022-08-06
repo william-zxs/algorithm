4f867ff19365e429bd74e79740eeb72df9305eac
william <william_zxs@163.com>
2022-08-06 11:46:13 +0800

    add 95. 不同的二叉搜索树 II

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -27,3 +27,38 @@ func swapPairs(head *ListNode) *ListNode {
 	}
 	return dummy.Next
 }
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+//95. 不同的二叉搜索树 II
+//中等
+func GenerateTrees(n int) []*TreeNode {
+	//迭代的思想
+	return helper(1, n)
+}
+
+func helper(start, end int) []*TreeNode {
+	if start > end {
+		return []*TreeNode{nil}
+	}
+	res := make([]*TreeNode, 0)
+	for i := start; i <= end; i++ {
+		leftList := helper(start, i-1)
+		rightList := helper(i+1, end)
+
+		for _, left := range leftList {
+			for _, right := range rightList {
+				root := &TreeNode{Val: i}
+				root.Left = left
+				root.Right = right
+				res = append(res, root)
+			}
+		}
+
+	}
+	return res
+}
