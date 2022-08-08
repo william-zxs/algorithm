cacc19b4d20c647063af16b5cf183634dc314e92
william <william_zxs@163.com>
2022-08-08 18:32:44 +0800

    add  438. 找到字符串中所有字母异位词

--- doublepointer/doublepointer.go
+++ doublepointer/doublepointer.go
@@ -117,3 +117,30 @@ func checkInclusion(s1 string, s2 string) bool {
 	return false
 
 }
+
+//438. 找到字符串中所有字母异位词
+func findAnagrams(s string, p string) []int {
+	if len(s) < len(p) {
+		return []int{}
+	}
+
+	var pArray [26]int
+	var sArray [26]int
+	for i, v := range p {
+		pArray[v-'a'] += 1
+		sArray[s[i]-'a'] += 1
+	}
+	res := make([]int, 0)
+	if pArray == sArray {
+		res = append(res, 0)
+	}
+	for i, j := 0, len(p); j < len(s); i, j = i+1, j+1 {
+		sArray[s[i]-'a'] -= 1
+		sArray[s[j]-'a'] += 1
+		if pArray == sArray {
+			res = append(res, i+1)
+		}
+	}
+	return res
+
+}
