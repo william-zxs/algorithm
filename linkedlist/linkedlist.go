d51a2295dce812af324c5cc856f6615d8d9e454f
william <william_zxs@163.com>
2022-08-24 19:00:17 +0800

    86. 分隔链表

--- linkedlist/linkedlist.go
+++ linkedlist/linkedlist.go
@@ -27,6 +27,27 @@ func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 	return dummy.Next
 }
 
+//86. 分隔链表
+func partition(head *ListNode, x int) *ListNode {
+	lDummy := &ListNode{}
+	rDummy := &ListNode{}
+	l := lDummy
+	r := rDummy
+	for head != nil {
+		if head.Val < x {
+			l.Next = head
+			l = l.Next
+		} else {
+			r.Next = head
+			r = r.Next
+		}
+		head = head.Next
+	}
+	l.Next = rDummy.Next
+	r.Next = nil
+	return lDummy.Next
+}
+
 func main() {
 
 }
